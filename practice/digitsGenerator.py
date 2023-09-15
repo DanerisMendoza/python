2bb2778a336a5303eb808898528103dc5315eb37
Daneris Mendoza <danerismendoza096@gmail.com>
2023-09-15 15:41:36 +0800

    digitsGenerator added

---
+++ practice/digitsGenerator.py
@@ -0,0 +1,5 @@
+import random
+
+digits = list(range(1,11))
+random.shuffle(digits)
+print(digits)
--- practice/mergeSort.py
+++ practice/mergeSort.py
@@ -16,5 +16,19 @@ def merge_sort(arr):
     sorted_arr = []
     left_index, right_index = 0, 0
 
+    while left_index < len(left_half_arr) and right_index < len(right_half_arr):
+        if left_half_arr[left_index] < right_half_arr[right_index]:
+            sorted_arr.append(left_half_arr[left_index])
+            left_index += 1
+        else
+            sorted_arr.append(right_half_arr[right_index])
+            right_index
+
+    sorted_arr.extend(left_half_arr[left_index:])
+    sorted_arr.extend(right_half[right_index:])
+
+    return sorted_arr
+
+# arr = [1,2,3,4,5,6,7,8,9,10]
 arr = [1,2,3,4,5,6,7,8,9,10]
 merge_sort(arr)
